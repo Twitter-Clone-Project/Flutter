5b1d4c81c1cb8518b37aa76bbc4eb0f37ce27694
MDR <119791309+MohammadAlomar8@users.noreply.github.com>
2023-10-22 19:47:57 +0300

    adding more test cases to signup_test.dart

--- test/features/auth/view/signup_test.dart
+++ test/features/auth/view/signup_test.dart
@@ -4,12 +4,12 @@ import 'package:x_clone/features/auth/view/signup_view.dart';
 
 void main() {
   testWidgets('Test SignUp widget', (WidgetTester tester) async {
-    // Build our widget and trigger a frame.
+
     await tester.pumpWidget(const MaterialApp(
       home: SignUp(),
     ));
 
-    // Use finder to locate specific widgets for testing.
+
     final createAccountText = find.text("Create your account");
     final signUpButton = find.text("Sign up");
     final fullNameTextField = find.byWidgetPredicate((widget) =>
@@ -19,19 +19,19 @@ void main() {
     final passwordTextField = find.byWidgetPredicate((widget) =>
     widget is TextField && widget.decoration?.labelText == 'Password');
 
-    // Verify that the widgets are present on the screen.
+
     expect(createAccountText, findsOneWidget);
     expect(signUpButton, findsOneWidget);
     expect(tester.widget<TextField>(fullNameTextField).controller!.text, '');
     expect(tester.widget<TextField>(emailTextField).controller!.text, '');
     expect(tester.widget<TextField>(passwordTextField).controller!.text, '');
 
-    // Test entering text into the text fields.
+
     await tester.enterText(fullNameTextField, 'John Doe');
     await tester.enterText(emailTextField, 'johndoe@example.com');
     await tester.enterText(passwordTextField, 'securepassword');
 
-    // Verify the text fields are updated correctly.
+
     expect(tester.widget<TextField>(fullNameTextField).controller!.text, 'John Doe');
     expect(tester.widget<TextField>(emailTextField).controller!.text, 'johndoe@example.com');
     expect(tester.widget<TextField>(passwordTextField).controller!.text, 'securepassword');
