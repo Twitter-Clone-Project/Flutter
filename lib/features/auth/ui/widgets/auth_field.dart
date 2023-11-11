7ddf49c7868cbf3cb666986f4761bbd13ea84b10
Yousef Rabia <yousef.mohamed.rabia@gmail.com>
2023-11-11 03:11:18 +0200

    update auth field

--- lib/features/auth/ui/widgets/auth_field.dart
+++ lib/features/auth/ui/widgets/auth_field.dart
@@ -1,48 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../theme/app_colors.dart';
 
-class AuthField extends StatelessWidget {
-  const AuthField({
+class AuthField extends ConsumerWidget {
+  const AuthField( {
     super.key,
     required this.labelText,
     this.controller,
     this.errorText,
     this.maxLength,
     this.validator,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.onChange,
   });
 
   final TextEditingController? controller;
   final int? maxLength;
   final String labelText;
   final String? errorText;
+  final bool obscureText;
+  final Widget? suffixIcon;
   final Color enabledColor = AppColors.borderDarkGray;
   final Color focusedColor = AppColors.primaryColor;
   final String? Function(String?)? validator;
+  final String? Function(String?)? onChange;
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
     return TextFormField(
       controller: controller,
         maxLength: maxLength,
-        decoration: InputDecoration(
-          labelText: labelText,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        suffixIcon: suffixIcon,
+        labelText: labelText,
           errorText: errorText,
           contentPadding: const EdgeInsets.all(18),
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: enabledColor),
           ),
           border: const OutlineInputBorder(),
-          suffixIcon: Visibility(
-            visible: errorText != null,
-            child: const Icon(
-              Icons.error,
-              color: Colors.red,
-            ),
-          ),
         ),
       validator: validator,
+      onChanged: onChange,
     );
   }
 }
