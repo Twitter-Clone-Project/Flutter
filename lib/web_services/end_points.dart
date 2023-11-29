d68df35e041024d94044c115773a54419f692c1b
MostafaHani <mostafahani150@gmail.com>
2023-11-29 02:47:24 +0200

    Tweet Likers List UI + Fetching Data From API

--- lib/web_services/end_points.dart
+++ lib/web_services/end_points.dart
@@ -6,5 +6,4 @@ class EndPoints {
   static const confirmEmail = "/auth/verifyEmail";
   static const forgetPassword = "/auth/forgetPassword";
   static const resetPassword = "/auth/resetPassword";
-  static const getLikers = "";
 }
