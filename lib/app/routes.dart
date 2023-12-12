6f310af2134220338ce95c4e967cc20eef757394
Moustafa <123744354+mou-code@users.noreply.github.com>
2023-12-12 02:51:56 +0200

    Update routes.dart

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -44,7 +44,7 @@ class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case initRoute:
-        return MaterialPageRoute(builder: (_) => const ProfileScreen(username: "mou",));
+        return MaterialPageRoute(builder: (_) => const AuthScreen());
       case introScreen:
         return MaterialPageRoute(builder: (_) => const IntroScreen());
       case addTweet:
