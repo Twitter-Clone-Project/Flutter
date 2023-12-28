45b4f535b8b15cbb9fd57bdc7f31ecdebcb6b531
Walid-Kh <walid.osama.khamees@gmail.com>
2023-12-28 03:19:37 +0200

    Doc: notification state

--- lib/features/notifications/data/state/notifications_state.dart
+++ lib/features/notifications/data/state/notifications_state.dart
@@ -5,6 +5,22 @@ import '../model/notifications.dart';
 
 part 'notifications_state.freezed.dart';
 
+/// Represents the state of the notifications feature.
+///
+/// This class is used to manage the state of the notifications feature in the application.
+/// It contains information such as the list of notifications, loading status, error message, screen index, and error field.
+/// The state is immutable and can only be modified by creating a new instance of this class.
+///
+/// Example usage:
+/// ```dart
+/// NotificationsState state = NotificationsState(
+///   notifications: NotificationsList(data: []),
+///   loading: true,
+///   errorMessage: null,
+///   screenIndex: 0,
+///   error: null,
+/// );
+/// ```
 @freezed
 class NotificationsState with _$NotificationsState {
   const factory NotificationsState({
