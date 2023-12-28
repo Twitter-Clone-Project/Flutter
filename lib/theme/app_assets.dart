79e85bb72d9fb62ae6e8fe6285fa7ce377479a5a
Walid-Kh <walid.osama.khamees@gmail.com>
2023-12-28 03:24:38 +0200

    Doc: app assets

--- lib/theme/app_assets.dart
+++ lib/theme/app_assets.dart
@@ -1,26 +1,67 @@
 import 'package:flutter/material.dart';
 
+/// A class that defines the paths of various assets used in the application.
 class AppAssets {
-  //TODO Define path of your assets
+  /// The path of the X logo asset.
   static String logo = "res/assets/x.svg";
+
+  /// The path of the Google icon asset.
   static String googleIcon = "res/assets/google.svg";
+
+  /// The path of the filled like icon asset.
   static String likeFilledIcon = "res/assets/like_filled.svg";
+
+  /// The path of the outlined like icon asset.
   static String likeOutlinedIcon = "res/assets/like_outlined.svg";
+
+  /// The path of the comment icon asset.
   static String commentIcon = "res/assets/comment.svg";
+
+  /// The path of the gray comment icon asset.
   static String grayCommentIcon = "res/assets/gray_comment.svg";
+
+  /// The path of the retweet icon asset.
   static String retweetIcon = "res/assets/retweet.svg";
+
+  /// The path of the gray retweet icon asset.
   static String grayRetweetIcon = "res/assets/gray_retweet.svg";
+
+  /// The path of the views icon asset.
   static String viewsIcon = "res/assets/views.svg";
+
+  /// The path of the verified icon asset.
   static String verifiedIcon = "res/assets/verified.svg";
+
+  /// The path of the location icon asset.
   static String locationIcon = "res/assets/location.svg";
+
+  /// The path of the gallery icon asset.
   static String galleryIcon = "res/assets/gallery.svg";
+
+  /// The path of the GIF icon asset.
   static String gifIcon = 'res/assets/gif.svg';
+
+  /// The path of the list icon asset.
   static String listIcon = 'res/assets/list.svg';
+
+  /// The path of the earth icon asset.
   static String earthIcon = 'res/assets/earth.svg';
+
+  /// The path of the menu icon asset.
   static String menuIcon = 'res/assets/menu_icon.svg';
+
+  /// The path of the share icon asset.
   static String shareIcon = 'res/assets/share_icon.svg';
+
+  /// The path of the bookmark icon asset.
   static String bookmarkIcon = 'res/assets/bookmark_icon.svg';
+
+  /// The path of the gray retweet icon asset.
   static String greyRetweetIcon = 'res/assets/greyRetweetIcon.svg';
+
+  /// The path of the green retweeted icon asset.
   static String greenRetweetedIcon = 'res/assets/greenRetweetedIcon.svg';
+
+  /// The path of the white X logo asset.
   static String whiteLogo = 'res/assets/X_logo.png';
 }
