6177588998a04a3115994581635226e81b808644
Walid-Kh <walid.osama.khamees@gmail.com>
2023-12-28 03:26:13 +0200

    Doc: http client

--- lib/web_services/http_client.dart
+++ lib/web_services/http_client.dart
@@ -3,10 +3,9 @@ import 'package:dio/dio.dart';
 import 'end_points.dart';
 import 'interceptors/main_interceptor.dart';
 
+/// A class that provides an HTTP client for making network requests.
 class HttpClient {
   static final Dio dio = initDio();
-
-
 
   static final _singleton = HttpClient._internal();
 
@@ -16,6 +15,18 @@ class HttpClient {
 
   factory HttpClient() => _singleton;
 
+  /// Initializes and configures the Dio HTTP client.
+  ///
+  /// The [initDio] method creates a new instance of Dio with the specified
+  /// base URL and timeout duration. It also adds interceptors for logging
+  /// request and response details, as well as a custom interceptor for
+  /// handling main requests.
+  ///
+  /// Example usage:
+  ///
+  /// ```dart
+  /// var dio = HttpClient.initDio();
+  /// ```
   static Dio initDio() {
     var dio = Dio(BaseOptions(
       baseUrl: baseUrl,
@@ -31,5 +42,4 @@ class HttpClient {
 
     return dio;
   }
-
 }
