77eebd61d55461487fe377d36a95624f369579ad
Walid-Kh <walid.osama.khamees@gmail.com>
2023-12-28 03:15:36 +0200

    Doc: custom text

--- lib/features/auth/ui/widgets/custom_text.dart
+++ lib/features/auth/ui/widgets/custom_text.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:x_clone/theme/app_colors.dart';
 
+/// Enum representing the size of the custom text.
 enum CustomTextSize { small, medium, large }
 
+/// A custom text widget that allows customization of the text size and color.
 class CustomText extends StatelessWidget {
   final String text;
   final CustomTextSize textSize;
   final Color color;
 
+  /// Constructs a [CustomText] widget.
+  ///
+  /// The [text] parameter is required and specifies the text to be displayed.
+  /// The [textSize] parameter is optional and defaults to [CustomTextSize.medium].
+  /// The [color] parameter is optional and defaults to [AppColors.whiteColor].
   const CustomText(
     this.text, {
     Key? key,
@@ -15,10 +22,12 @@ class CustomText extends StatelessWidget {
     this.color = AppColors.whiteColor,
   }) : super(key: key);
 
+  /// Returns the font weight based on the [textSize].
   FontWeight _getFontWeight() {
     return textSize == CustomTextSize.large ? FontWeight.w800 : FontWeight.w400;
   }
 
+  /// Returns the font size based on the [textSize].
   double _getFontSize() {
     switch (textSize) {
       case CustomTextSize.small:
