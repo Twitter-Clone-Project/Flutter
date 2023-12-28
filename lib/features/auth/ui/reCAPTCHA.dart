975645bb33ca5088b051775d14cdbcf17098937c
Walid-Kh <walid.osama.khamees@gmail.com>
2023-12-28 03:13:20 +0200

    Doc: Captcha

--- lib/features/auth/ui/reCAPTCHA.dart
+++ lib/features/auth/ui/reCAPTCHA.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+/// A widget that displays a reCAPTCHA web page in a WebView.
+///
+/// The reCAPTCHA widget is used to verify that the user is a human and not a robot.
+/// It displays a web page with a reCAPTCHA challenge and listens for the response
+/// from the JavaScript code running in the web page.
+///
+/// Example usage:
+/// ```dart
+/// reCAPTCHA(
+///   key: UniqueKey(),
+/// )
+/// ```
 class reCAPTCHA extends StatelessWidget {
   const reCAPTCHA({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +31,6 @@ class reCAPTCHA extends StatelessWidget {
                   Navigator.of(context).pop(message.message);
                 })
           },
-
         ),
       ),
     );
