b65604ee6830d532f6bc5214ea607d1415819265
Walid-Kh <walid.osama.khamees@gmail.com>
2023-12-28 03:14:10 +0200

    Doc: change email otp screen

--- lib/features/auth/ui/settings_ui/change_email_otp_screen.dart
+++ lib/features/auth/ui/settings_ui/change_email_otp_screen.dart
@@ -6,8 +6,6 @@ import 'package:x_clone/features/auth/ui/widgets/auth_field.dart';
 import 'package:x_clone/features/auth/ui/widgets/custom_button.dart';
 import 'package:x_clone/features/auth/ui/widgets/custom_text.dart';
 
-
-
 import '../../../../app/app_keys.dart';
 import '../../../../app/routes.dart';
 import '../../../../theme/app_assets.dart';
@@ -15,16 +13,23 @@ import '../../../../theme/app_colors.dart';
 import '../../../../utils/utils.dart';
 import '../../data/providers/auth_provider.dart';
 
-class ChangeEmailOtpScreen extends StatefulHookConsumerWidget{
- final String email;
-const ChangeEmailOtpScreen({super.key,required this.email});
+/// A screen widget for entering and verifying the OTP (One-Time Password)
+/// for changing the email address.
+///
+/// This screen displays a form with an input field for entering the OTP code
+/// and a timer that counts down the remaining time to resend the OTP.
+/// It also handles the logic for updating the email address and resending the OTP.
+class ChangeEmailOtpScreen extends StatefulHookConsumerWidget {
+  final String email;
+  const ChangeEmailOtpScreen({super.key, required this.email});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => ChangeEmailOtpScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      ChangeEmailOtpScreenState();
 }
 
 class ChangeEmailOtpScreenState extends ConsumerState<ChangeEmailOtpScreen> {
-  int _resendTimer = 120 ;// Set the initial timer duration in seconds
+  int _resendTimer = 120; // Set the initial timer duration in seconds
   late Timer _timer;
   final TextEditingController _otpController = TextEditingController();
 
@@ -32,12 +37,12 @@ class ChangeEmailOtpScreenState extends ConsumerState<ChangeEmailOtpScreen> {
   void initState() {
     super.initState();
 
+    // Update the email address immediately after the screen is initialized
     Future.delayed(const Duration(seconds: 0), () {
       ref
           .read(authNotifierProvider.notifier)
           .updateEmail(newEmail: widget.email);
     });
-
 
     startResendTimer();
   }
@@ -60,7 +65,6 @@ class ChangeEmailOtpScreenState extends ConsumerState<ChangeEmailOtpScreen> {
   ///
   /// In the above example, the `startResendTimer` method is called on an instance
   /// of the `ChangeEmailOTPScreen` class, which starts the timer to resend the OTP.
-  ///```
   void startResendTimer() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
@@ -81,14 +85,13 @@ class ChangeEmailOtpScreenState extends ConsumerState<ChangeEmailOtpScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     final auth = ref.watch(authNotifierProvider);
     ref.listen(authNotifierProvider.select((value) => value.errorMessage),
-            (previous, nextErrorMessage) {
-          if (nextErrorMessage != null && nextErrorMessage != '') {
-            AppSnackbar.show(buildSnackBar(text: nextErrorMessage));
-          }
-        });
+        (previous, nextErrorMessage) {
+      if (nextErrorMessage != null && nextErrorMessage != '') {
+        AppSnackbar.show(buildSnackBar(text: nextErrorMessage));
+      }
+    });
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -103,110 +106,119 @@ class ChangeEmailOtpScreenState extends ConsumerState<ChangeEmailOtpScreen> {
               color: AppColors.whiteColor,
             ),
           ),
-          title:
-          SvgPicture.asset(
+          title: SvgPicture.asset(
             AppAssets.logo,
             height: 40,
             width: 40,
-            colorFilter:const ColorFilter.mode(AppColors.whiteColor, BlendMode.srcIn),
+            colorFilter:
+                const ColorFilter.mode(AppColors.whiteColor, BlendMode.srcIn),
           ),
           centerTitle: true,
           backgroundColor: AppColors.pureBlack,
         ),
-
-        body:  Form(
+        body: Form(
           key: AppKeys.verifyOtpFormKey,
-          child:Padding(
+          child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child:SingleChildScrollView(
-                child:SizedBox(
-                  height: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top,
-                  child:Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            child: SingleChildScrollView(
+                child: SizedBox(
+              height: MediaQuery.of(context).size.height -
+                  MediaQuery.of(context).padding.top,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const CustomText(
+                    "Check your email\nWe have sent you a otp code",
+                    textSize: CustomTextSize.large,
+                  ),
+                  SizedBox(height: 0.075 * MediaQuery.of(context).size.height),
+                  AuthField(
+                    controller: _otpController,
+                    labelText: "OTP",
+                    maxLength: 8,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "OTP cannot be empty";
+                      }
+                      return null;
+                    },
+                  ),
+                  Row(
                     children: [
                       const CustomText(
-                        "Check your email\nWe have sent you a otp code",
-                        textSize: CustomTextSize.large,
+                        "Don't get OTP?",
+                        textSize: CustomTextSize.small,
                       ),
-                      SizedBox(height:0.075 * MediaQuery.of(context).size.height),
-                      AuthField(controller: _otpController,labelText: "OTP",maxLength: 8,
-                        validator: (value){
-                          if(value!.isEmpty){
-                            return "OTP cannot be empty";
-                          }
-                          return null;
-                        },
-                      ),
-                      Row(
-                        children: [
-                          const CustomText("Don't get OTP?",
-                            textSize: CustomTextSize.small,
-                          ),
-                          TextButton(
-                            onPressed: _resendTimer > 0
-                                ? null // Disable the button when timer is active
-                                : () {
-                              ref.read(authNotifierProvider.notifier).resendOtp(email: widget.email).then((value) => AppSnackbar.show(buildSnackBar(text: value.toString(),backgroundColor: AppColors.primaryColor)));
-                              setState(() {
-                                _resendTimer = 120; // Reset the timer
-                              });
-                              startResendTimer(); // Start the timer
-                            },
-                            child: CustomText(
-                              _resendTimer > 0
-                                  ? '$_resendTimer second' // Display the remaining time
-                                  : 'Resend!',
-                              textSize: CustomTextSize.small,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height:0.015 * MediaQuery.of(context).size.height),
-                      auth.otpLoading?
-                      const SizedBox(
-                        width: 55,
-                        height: 55,
-                        child: CircularProgressIndicator(
-                          color: AppColors.whiteColor,
-                          strokeWidth: 1,
-                        ),
-                      )
-                          :SizedBox(
-                        height: 55,
-                        child:
-                        CustomButton(
-                          onPressed:()  async {
-                            if (AppKeys.verifyOtpFormKey.currentState!
-                                .validate()) {
-                              final result = await ref
-                                  .read(authNotifierProvider.notifier)
-                                  .confirmEmail(
-                                  isSignup: true,
-                                  otp: _otpController.text,
-                                  email: ref.read(authNotifierProvider).user!.email??'',
-                                newEmail: widget.email
-                              );
-
-                              if (result) {
-                                  Navigator.pushNamedAndRemoveUntil(context, Routes.initRoute, (route) => false);
-                              }
-                            }
-                          },
-                          text: 'Verify Otp',
+                      TextButton(
+                        onPressed: _resendTimer > 0
+                            ? null // Disable the button when timer is active
+                            : () {
+                                ref
+                                    .read(authNotifierProvider.notifier)
+                                    .resendOtp(email: widget.email)
+                                    .then((value) => AppSnackbar.show(
+                                        buildSnackBar(
+                                            text: value.toString(),
+                                            backgroundColor:
+                                                AppColors.primaryColor)));
+                                setState(() {
+                                  _resendTimer = 120; // Reset the timer
+                                });
+                                startResendTimer(); // Start the timer
+                              },
+                        child: CustomText(
+                          _resendTimer > 0
+                              ? '$_resendTimer second' // Display the remaining time
+                              : 'Resend!',
+                          textSize: CustomTextSize.small,
                         ),
                       ),
-                      SizedBox(height: 0.029 * MediaQuery.of(context).size.height),
-
                     ],
                   ),
-                )
-            ),
+                  SizedBox(height: 0.015 * MediaQuery.of(context).size.height),
+                  auth.otpLoading
+                      ? const SizedBox(
+                          width: 55,
+                          height: 55,
+                          child: CircularProgressIndicator(
+                            color: AppColors.whiteColor,
+                            strokeWidth: 1,
+                          ),
+                        )
+                      : SizedBox(
+                          height: 55,
+                          child: CustomButton(
+                            onPressed: () async {
+                              if (AppKeys.verifyOtpFormKey.currentState!
+                                  .validate()) {
+                                final result = await ref
+                                    .read(authNotifierProvider.notifier)
+                                    .confirmEmail(
+                                        isSignup: true,
+                                        otp: _otpController.text,
+                                        email: ref
+                                                .read(authNotifierProvider)
+                                                .user!
+                                                .email ??
+                                            '',
+                                        newEmail: widget.email);
+
+                                if (result) {
+                                  Navigator.pushNamedAndRemoveUntil(context,
+                                      Routes.initRoute, (route) => false);
+                                }
+                              }
+                            },
+                            text: 'Verify Otp',
+                          ),
+                        ),
+                  SizedBox(height: 0.029 * MediaQuery.of(context).size.height),
+                ],
+              ),
+            )),
           ),
         ),
       ),
     );
   }
-
 }
-
-
